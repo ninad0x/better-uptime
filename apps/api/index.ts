cabb85eb047f197c2fbc3779cc06b96fab28dd5b
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-07 22:50:25 +0530

    add user signup/signin routes

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,9 @@
+import jwt from "jsonwebtoken";
 import express from "express"
 import { prisma } from "store/client"
+import "dotenv/config";
+import { AuthInput } from "./types";
+
 
 const app = express()
 app.use(express.json())
@@ -9,14 +13,82 @@ app.get("/", (req, res) => {
 })
 
 
-app.post("/website", async (req, res) => {
+app.post("/user/signup", async (req, res) => {
+    try {
 
-    const websites = await prisma.website.findMany()
+        const data = AuthInput.safeParse(req.body)
 
-    return res.json({
-        websites
-    })
+        if (!data.success) {
+            return res.status(403).send("")
+        }
+
+        const user = await prisma.users.create({
+            data: {
+                username: data.data.username,
+                password: data.data.password
+            }
+        })
+
+        return res.json({
+            id: user.id
+        })
+
+    } catch (error) {
+        return res.status(403).send("")
+    }
 })
+
+
+app.post("/user/signin", async (req, res) => {
+    try {
+        const data = AuthInput.safeParse(req.body)
+
+        if (!data.success) {
+            return res.status(403).send("not")
+        }
+
+        let user = await prisma.users.findFirst({
+            where: {
+                username: data.data.username,
+            }
+        })
+
+        if (!user) {
+            return res.json({
+                message: "User does not exist"
+            })
+        }
+
+        if (user.password !== data.data.password) {
+            return res.json({
+                message: "Incorrect password"
+            })
+        }
+
+        const token = jwt.sign(user.id, process.env.JWT_SECRET!)
+
+        res.json({
+            jwt: token
+        })
+
+
+    } catch (error) {
+        return res.status(403).send("err")
+    }
+})
+
+// app.post("/website", async (req, res) => {
+    
+//     const website = await prisma.website.create({
+//         data: {
+            
+//         }
+//     })
+
+//     return res.json({
+//         id: website.id
+//     })
+// })
 
 
 app.listen(3001)
