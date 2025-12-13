c46ed3cce3298deb899fdafea486e0a9c046ae1b
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-13 18:06:33 +0530

    fix: add regions: us-east-1 and eu-west-1

--- apps/cdk/bin/cdk.ts
+++ apps/cdk/bin/cdk.ts
@@ -1,11 +1,19 @@
-#!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { UptimeStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new UptimeStack(app, "UptimeStack", {
-  env: { 
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: "ap-south-1"
-  }
-});
+const regions = [
+  "ap-south-1",
+  "us-east-1",
+  "eu-west-1",
+];
+
+regions.forEach((region) => {
+  new UptimeStack(app, `UptimeStack-${region}`, {
+    env: { 
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: region
+    }
+  });
+
+})
