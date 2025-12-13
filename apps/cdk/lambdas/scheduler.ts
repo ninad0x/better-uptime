18a46a6ffe69d84ac019d39eb4ab53fa344ccd4b
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-13 18:05:20 +0530

    fix: update lambda fn

--- apps/cdk/lambdas/scheduler.ts
+++ apps/cdk/lambdas/scheduler.ts
@@ -1,26 +1,26 @@
 const { SQSClient, SendMessageCommand } = require("@aws-sdk/client-sqs");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
 
-const sqs = new SQSClient();
-
+const sqs = new SQSClient({});
+const ssm = new SSMClient({});
 
 exports.handler = async () => {
-  
-  const response = await fetch("https://9d96ad64301a.ngrok-free.app/websites")
-  const data = await response.json()
-  
-  console.log(data);
+  const startRes = await ssm.send(
+    new GetParameterCommand({ Name: "/uptime/start" })
+  );
 
-  const websites = (data as any).websites.map((w: any) => w.url)
+  if (startRes.Parameter?.Value !== "true") return "PAUSED";
 
-  console.log(websites);
+  const response = await fetch("https://803841669d26.ngrok-free.app/websites");
+  const data = await response.json();
+  const websites = (data as any).websites.map((w:any) => w.url);
 
   await sqs.send(
     new SendMessageCommand({
       QueueUrl: process.env.QUEUE_URL,
-      MessageBody: JSON.stringify(websites)
+      MessageBody: JSON.stringify(websites),
     })
   );
-
 
   return "OK";
 };
--- apps/cdk/lambdas/worker.ts
+++ apps/cdk/lambdas/worker.ts
@@ -31,7 +31,7 @@ exports.handler = async (event: any) => {
 
   }
 
-  await fetch("https://9d96ad64301a.ngrok-free.app/uptime", {
+  await fetch("https://803841669d26.ngrok-free.app/uptime", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
