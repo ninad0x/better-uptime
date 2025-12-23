dba74260323e6c5f1431e054316ba8c51cedf3c0
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-23 23:19:55 +0530

    test file

---
+++ apps/api/test-time.ts
@@ -0,0 +1,5 @@
+
+const start = new Date("2025-12-23T17:35:03.463Z")
+const end = new Date("2025-12-23T17:37:40.441Z")
+console.log(start.toLocaleTimeString());
+console.log(end.toLocaleTimeString());
