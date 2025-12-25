237712e5a79613a3ccb47daed704054ec5bb2dec
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-25 22:46:21 +0530

    chore: update delay:25 sec and window:6 mins

--- apps/api/cron/aggregator.ts
+++ apps/api/cron/aggregator.ts
@@ -35,23 +35,24 @@ function computeMetrics(ticks: {
 }
 
 
-export const aggregateTick = schedule("*/3 * * * *", async () => {
+export const aggregateTick = schedule("*/6 * * * *", async () => {
 
     console.log("\nCRON + delay started", new Date().toLocaleTimeString());
-    await new Promise(r => setTimeout(r, 30_000))  // delay 30 sec to match all regions
+    await new Promise(r => setTimeout(r, 25_000))  // delay to sync all regions
     console.log("delay end",new Date().toLocaleTimeString());
 
-    const WINDOW_MS = 3 * 60 * 1000;  // time window 2 mins
+    const WINDOW_MS = 6 * 60 * 1000;  // time window  mins
     const now = Date.now();
 
     const windowEnd = new Date(Math.floor(now / WINDOW_MS) * WINDOW_MS);
+
     const windowStart = new Date(windowEnd.getTime() - WINDOW_MS);
 
     const ticks = await prisma.websiteTick.findMany({
         where: {
             created_at: {
                 // gte: new Date("2025-12-15T16:00:26.136Z"),
-                // lte: new Date("2025-12-15T16:03:56.766Z"),
+                // lt: new Date("2025-12-15T16:03:56.766Z"),
                 gte: windowStart,
                 lt: windowEnd,
             },
@@ -91,5 +92,6 @@ export const aggregateTick = schedule("*/3 * * * *", async () => {
     })
 
     console.log("created at", websiteMetric[0]?.created_at.toLocaleTimeString());
+    console.log("\n\n");
 
 })
