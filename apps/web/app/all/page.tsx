8dacc21ee6eedf7f75e682f5eac7679a1f726e09
Ninad09 <ninadgaonkar234@gmail.com>
2025-12-30 23:30:40 +0530

    feat: add shadcn navigation pane

--- apps/web/app/all/page.tsx
+++ apps/web/app/all/page.tsx
@@ -1,13 +1,11 @@
 export default async function GetAll() {
 
-    const res = await fetch("http://localhost:3001/all")
+    const res = await fetch(`${process.env.BACKEND_URL}/all`)
     const data = await res.json()
-    console.log(data.ticksData);
-
     return (
     <div>
         {data.ticksData?.map((e: any) => (
-            <div key={e.id} className="p-2">
+            <div key={e.id} className="p-1">
                 <h3>{e.url}{" - "}{e.status}{" - "}{e.createdAt}</h3>
             </div>
         ))}
