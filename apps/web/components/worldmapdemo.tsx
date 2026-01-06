b67122a3cba542656ce27c3ec8f1d01b95c22d72
Ninad09 <ninadgaonkar234@gmail.com>
2026-01-06 23:27:14 +0530

    feat: add dotted worldmap component for hero section

--- apps/web/components/worldmapdemo.tsx
+++ apps/web/components/worldmapdemo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import WorldMap from "@/components/world-map";
+import WorldMap from "@/components/worldmap";
 import { HeroText } from "./herotext";
 
 export function WorldMapDemo() {
@@ -8,13 +8,9 @@ export function WorldMapDemo() {
 
     <HeroText />
 
-    <div className="opacity-70 mask-b-from-50% mask-b-to-65%">
+    <div className="opacity-90 mask-t-from-85% mask-b-from-50% mask-b-to-65%">
       <WorldMap
               dots={[
-              // {
-              //     start: { lat: 64.2008, lng: -149.4937, }, // Alaska (Fairbanks)
-              //     end: { lat: 34.0522, lng: -118.2437, }, // Los Angeles
-              // },
               {
                   start: { lat: 64.2008, lng: -149.4937 }, // Alaska (Fairbanks)
                   end: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
@@ -23,10 +19,6 @@ export function WorldMapDemo() {
                   start: { lat: -15.7975, lng: -47.8919 }, // Brazil (Brasília)
                   end: { lat: 38.7223, lng: -9.1393 }, // Lisbon
               },
-              // {
-              //     start: { lat: 51.5074, lng: -0.1278 }, // London
-              //     end: { lat: 28.6139, lng: 77.209 }, // New Delhi
-              // },
               {
                   start: { lat: 28.6139, lng: 77.209 }, // New Delhi
                   end: { lat: 43.1332, lng: 131.9113 }, // Vladivostok
