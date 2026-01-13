76a2942ee5ead603d4d640c6bd85ed2fac22ae01
Ninad09 <ninadgaonkar234@gmail.com>
2026-01-13 23:16:31 +0530

    dashboard update, add website table for calculated metrics

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,16 +1,40 @@
-import { serverApi } from '@/lib/api';
-import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { getDashboardData } from "@/lib/getDashboardData";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
+
+export const dynamic = "force-dynamic";
 
 export default async function Dashboard() {
 
-    const api = await serverApi()
-    const { data } = await api.get("/api/profile")
+  const data = await getDashboardData();
 
-    console.log(data.user.name);
-
-    return (
-    <div className="flex flex-col items-center h-screen mx-auto mt-10">
-        Hello {JSON.stringify(data.user.name)}
-    </div>
-    )
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          
+          <div className="bg-muted/50 min-h-screen flex-1 rounded-xl md:min-h-min">
+            <DataTable columns={columns} data={data}/>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
