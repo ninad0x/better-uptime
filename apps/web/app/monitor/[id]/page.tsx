c2a833c538bcc98bdea504d4e1becc5c5cb99297
Ninad09 <ninadgaonkar234@gmail.com>
2026-01-15 22:32:06 +0530

    monitor page

--- apps/web/app/monitor/[id]/page.tsx
+++ apps/web/app/monitor/[id]/page.tsx
@@ -1,10 +1,25 @@
+import { getWebsiteDetails } from "@/lib/monitorData"
 
 
-export default async function MonitorPgage({params}: {params: Promise<{ id: string}>}) {
+export default async function MonitorPage({params} : 
+    { params: Promise<{ id: string}> }) {
 
     const websiteId = (await params).id
 
-    return <div>
-        <h1>{websiteId}</h1>
+    const data = await getWebsiteDetails(websiteId)
+
+    if (!data) return <p>No data avaliable</p>
+
+    return <div className="p-4 h-screen bg-zinc-100">
+        <h2>{data.name}</h2>
+        <p>{data.url}</p>
+        <p>{data.isActive ? "yes" : "no"}</p>
+        <p>{data.lastChecked?.toLocaleTimeString()}</p>
+
+        {data.ticks.map((e) => {
+            return <div key={e.id}>
+                
+            </div>
+        })}
     </div>
 }
