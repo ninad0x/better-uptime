61f2c781d5056749ba0d3e215d341c32fc47c75c
Ninad09 <ninadgaonkar234@gmail.com>
2026-01-23 23:15:45 +0530

    helper function to fetch sites

--- apps/web/lib/getDashboardData.ts
+++ apps/web/lib/getDashboardData.ts
@@ -1,42 +1,17 @@
-import { prisma } from "store/client";
+import { prisma } from "@repo/db/client";
 
-export async function getDashboardData() {
-  const timeWindow = new Date();
-  timeWindow.setHours(timeWindow.getHours() - 100); 
+export async function getDashboardData(userId: string) {
+  const oneDayAgo = new Date();
+  oneDayAgo.setHours(oneDayAgo.getHours() - 24);
 
-  const sites = await prisma.website.findMany({
-    orderBy: { id: 'asc' }, 
+  return await prisma.website.findMany({
+    where: { userId },
     include: {
-      metric: {
-        where: { createdAt: { gte: timeWindow } },
+      ticks: {
+        where: { createdAt: { gte: oneDayAgo } },
         orderBy: { createdAt: 'asc' },
+        select: { status: true, createdAt: true }
       }
     }
-  });
-
-  return sites.map((site) => {
-  
-    const lastMetric = site.metric[site.metric.length - 1];
-    
-    const statusText = lastMetric?.finalStatus ?? "Pending";
-
-    const upCount = site.metric.filter(m => m.finalStatus === "Up").length;
-    const totalCount = site.metric.length;
-
-    const uptimePercent = totalCount > 0 ? (upCount / totalCount) * 100 : 0;
-    console.log(site.name, uptimePercent);
-
-    const avgList = site.metric.map((s) => s.avgResponseTimeMs ?? 0)
-    
-    const newAvgResTime = avgList.reduce((a, b) => a + b, 0) / avgList.length
-
-    return {
-      id: site.id,
-      url: site.url,
-      currentStatus: statusText,
-      uptime: uptimePercent.toFixed(1),
-      sparkline: site.metric.map(m => m.avgResponseTimeMs ?? 0),
-      avgResponseTime: newAvgResTime.toFixed(1)
-    };
   });
 }
