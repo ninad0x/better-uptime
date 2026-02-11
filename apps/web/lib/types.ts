db2f42ea54600292a5ee45f29dfee74129714461
Ninad09 <ninadgaonkar234@gmail.com>
2026-02-11 23:51:45 +0530

    add website tick schema

--- apps/web/lib/types.ts
+++ apps/web/lib/types.ts
@@ -11,5 +11,22 @@ export const signInSchema = z.object({
     password: z.string().min(8, "Password must be atleast 8 characters"),
 })
 
+const WebsiteTickSchema = z.object({
+    id: z.string(),
+    website: z.string(),
+    status: z.number(),
+    latency: z.number().nullable(),
+    timestamp: z.number(),
+    details: z.string()
+})
+
+export const WebsiteTickBatch = z.object({
+    region: z.string(),
+    results: z.array(WebsiteTickSchema)
+})
+
 export type signUpValues = z.infer<typeof signUpSchema>
 export type signInValues = z.infer<typeof signInSchema>
+export type WebsiteTickType = z.infer<typeof WebsiteTickSchema>
+
+
